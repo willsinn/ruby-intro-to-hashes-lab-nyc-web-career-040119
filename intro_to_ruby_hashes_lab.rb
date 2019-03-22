1624cdce1754369654acba12cce2efe163fd65bc
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 15:13:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-    railroads: {
+    :railroads: {
       pieces: 4, 
-      names: {:pennsylvania_railroad}, 
+      names: {}, 
       rent_in_dollars: {
         :one_piece_owned => 25, 
         :two_pieces_owned => 50, 
