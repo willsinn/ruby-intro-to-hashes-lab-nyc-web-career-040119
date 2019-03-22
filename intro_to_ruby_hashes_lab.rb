584500ecf97188c96376d746c3c3685740565111
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 15:15:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,8 @@ def monopoly_with_third_tier
       pieces: 4, 
       names: {
         :pennsylvania_railroad => {},
+        :reading_railroad => {},
+
       }, 
       rent_in_dollars: {
         :one_piece_owned => 25, 
